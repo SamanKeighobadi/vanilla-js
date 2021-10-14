d6215c555306d555b2078a00d447a98f71d861e8
saman keighobadi <saman.keighobadi@gmail.com>
2021-10-14 12:47:27 +0330

    javascript added

---
+++ TabNavigation/index.js
@@ -0,0 +1,15 @@
+const itmes = document.querySelectorAll('.cd-tabs-navigation li a')
+itmes.forEach((item) => {
+    item.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+        document.querySelector(".cd-tabs-navigation li a.selected").classList.remove('selected')
+        this.classList.add('selected')
+        
+        let tabs = this.getAttribute('data-content')
+        
+        document.querySelector(".cd-tabs-content li.selected").classList.remove('selected')
+        document.querySelector(`.cd-tabs-content li[data-content=${tabs}]`).classList.add('selected')
+    })
+})
