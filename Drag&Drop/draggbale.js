1c4ee6f2939b3cde053ae281d574e8243892b9ef
saman keighobadi <saman.keighobadi@gmail.com>
2021-10-14 11:34:44 +0330

    inti new project

--- Drag&Drop/draggbale.js
+++ Drag&Drop/draggbale.js
@@ -1,18 +1,78 @@
 class draggable {
-  constructor(props) {
-    this.setUpList(props);
+  dragSrcEl;
+  list;
+  constructor(options) {
+    this.setupList(options);
+
+    this.list = options.list;
+
+    for (let listItem of options.el.children) {
+      this.addDnDHandlers(listItem);
+    }
   }
 
-  setUpList(props) {
-    let { list, el, template } = props;
+  setupList(options) {
+    let { list, el: element, template } = options;
 
-    if (!el) throw Error("the list is not exists");
+    if (!element) throw Error("the list is not exists");
     if (!list) throw Error("the data is not exists");
     if (!Array.isArray(list))
-      throw Error("the list is not an array please enter an array");
-    if (!template) throw Error("the templage is not exist");
-    if (typeof template !== "function") throw Error("the templage is function");
+      throw Error("the list is not an array, please insert an array");
+    if (!template) throw Error("please add a Tempalte function");
+    if (typeof template !== "function")
+      throw Error("please add a function as template");
 
-    list.forEach((item) => (el.innerHTML += template(item)));
+    list.forEach((item) => (element.innerHTML += template(item)));
+  }
+
+  addDnDHandlers(element) {
+    element.setAttribute("draggable", true);
+
+    element.addEventListener("dragstart", this.handleDragStart.bind(this));
+    element.addEventListener("drageneter", this.handleDragEneter.bind(this));
+    element.addEventListener("dragover", this.handleDragOver.bind(this));
+    element.addEventListener("dragleave", this.handleDragLeave.bind(this));
+    element.addEventListener("drop", this.handleDragDrop.bind(this));
+    element.addEventListener("dragend", this.handleDragEnd.bind(this));
+  }
+
+  handleDragStart(e) {
+    this.dragSrcEl = e.target;
+
+    e.dataTransfer.setData("text/html", e.target.outerHTML);
+
+    e.target.classList.add("dragElem");
+  }
+
+  handleDragEneter(e) {}
+
+  handleDragOver(e) {
+    if (e.preventDefault) e.preventDefault();
+
+    e.target.classList.add("over");
+  }
+
+  handleDragLeave(e) {
+    e.target.classList.remove("over");
+  }
+
+  handleDragDrop(e) {
+    let target = e.target.closest(".list-item");
+
+    if (this.dragSrcEl != target) {
+      target.parentNode.removeChild(this.dragSrcEl);
+      let dropHTML = e.dataTransfer.getData("text/html");
+      target.insertAdjacentHTML("beforebegin", dropHTML);
+      this.addDnDHandlers(target.previousSibling);
+    }
+    e.target.classList.remove("over");
+  }
+
+  handleDragEnd(e) {
+    e.target.classList.remove("dragElem");
+
+    let newList;
+    list.querySelectorAll('.list-itme').forEach(elm => newList.push(this.list.find(item => elm.id === item.id)))
+console.log(newList)
   }
 }
--- Drag&Drop/index.js
+++ Drag&Drop/index.js
@@ -2,52 +2,52 @@ let data = [
     {
       id: 1,
       title: "React.js",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 2,
       title: "Vue.js",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 3,
       title: "Node.js",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 4,
       title: "Angular",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 5,
       title: "TailwindCSS",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 6,
       title: "Bootstrap",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 7,
       title: "Express.js",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 8,
       title: "CSS",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 9,
       title: "HTML",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
     {
       id: 10,
       title: "Javascript",
-      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. Eu veniam duis ipsum mollit aliquip laborum quis qui elit irure.",
+      text: "Incididunt esse amet laboris consectetur qui. Eu velit magna ea mollit nulla incididunt. Pariatur ad ipsum ex cillum dolore elit est consequat reprehenderit do anim. ",
     },
   ];
   
